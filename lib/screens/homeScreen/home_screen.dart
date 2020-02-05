5549778ea9c34154145b3d66c0218a4543078f18
Miller César de Oliveira <miller00315@gmail.com>
2020-02-05 10:18:40 -0300

    correção

--- lib/screens/homeScreen/home_screen.dart
+++ lib/screens/homeScreen/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:pokedex/models/poke_api.dart';
-import 'package:pokedex/screens/homeScreen/details/poke_detail_screen.dart';
+import 'package:pokedex/screens/details/poke_detail_screen.dart';
 import 'package:pokedex/screens/homeScreen/widgets/poke_item.dart';
 import 'package:provider/provider.dart';
 import '../../consts/consts_app.dart';
